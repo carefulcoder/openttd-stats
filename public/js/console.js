376f65300678e14dd4d5928128d54e9d09712a34
tom <javaguychronox@gmail.com>
2013-07-09 20:22:51 +0100

    Tiny style fixes, only pushing because Github has swallowed one of my commits :/

--- public/js/console.js
+++ public/js/console.js
@@ -1,10 +1,14 @@
 var socket = io.connect('http://localhost');
 
 socket.on('err', function (data) {
-  var line = document.createTextNode(data.data + "\n");
+  var line = document.createElement('span');
+  line.appendChild(document.createTextNode(data.data + "\n"));
+  line.className = 'error';
   document.getElementById('code').appendChild(line);
 });
 socket.on('out', function (data) {
-    var line = document.createTextNode(data.data + "\n");
+    var line = document.createElement('span');
+    line.appendChild(document.createTextNode(data.data + "\n"));
+    line.className = 'out';
     document.getElementById('code').appendChild(line);
 });
